8ad97338dd875cd30bdca6eb65434149d95bb34c
Harigaran <111054655+harikngp@users.noreply.github.com>
2022-08-29 19:08:12 +0530

    Delete addit directory

--- addit/app/Controllers/Http/CntrlsController.ts
+++
@@ -1,9 +0,0 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
-export default class CntrlsController {
-    public async arith({request}){
-        var a:number=request.input('a')
-        var b:number=request.input('b')
-        return a+b
-    }
-}
