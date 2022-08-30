879016f98fae0b5c5651a21a57ec772f034cf5f4
harigaran <harigaran.k@ngpwebsmart.com>
2022-08-30 10:53:42 +0530

    ts task

---
+++ ts_arith.ts
@@ -0,0 +1,9 @@
+let a:number=10,b:number=5,add,sub,mul,div;
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+console.log('Addition:'+add);
+console.log('Subtraction:'+sub);
+console.log('Multiplication:'+mul);
+console.log('Division'+div);
