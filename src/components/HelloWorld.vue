226c884423bc453c04de0c64b1405f52cff00b09
harigaran <harigaran.k@ngpwebsmart.com>
2022-09-06 19:53:27 +0530

    vue task

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,89 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
+  <v-app>
+    <v-form ref="form" v-model="valid" lazy-validation>
 
-<script>
+    <v-text-field v-model="name" :rules="nameRules" :counter=30 label="Name" 
+      required>Name</v-text-field>
+
+    <h3>Gender</h3>
+    <v-radio-group v-model="gender" column>
+      <v-radio label="male" value="Male"></v-radio>
+      <v-radio label="female" value="Female"></v-radio>
+    </v-radio-group>
+
+    <h3>Hobbies</h3>
+    <v-checkbox v-model="hobbies" label="Cricket" value="Cricket">
+      </v-checkbox>
+    <v-checkbox v-model="hobbies" label="Music" value="Music">
+      </v-checkbox>
+    <v-checkbox v-model="hobbies" label="Reading" value="Reading">
+      </v-checkbox>
+    <v-checkbox v-model="hobbies" label="Others" value="Others">
+      </v-checkbox>
+
+    <v-select v-model="city" :items="city"
+      :rules="[v => !!v || 'required']"
+      label="City" required>
+    </v-select>
+
+    <v-text-field v-model="email" :rules="emailRules" label="Email"
+      required></v-text-field>
+
+    <v-text-field v-model="phone" :rules="phoneRules" :counter=10 label="Phone Number" 
+      required></v-text-field>
+   
+    <v-btn :disabled="!valid" color="success" @click="validate">
+      Submit</v-btn>
+   </v-form>    
+</v-app>
+</template>
+<script lang="ts">
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+     data: () => ({
+       valid: true,
+       name: '',
+       nameRules: [
+         name=>!!name||'required',
+         v=>v.length>3 || 'Invalid name'
+       ],
+       email: '',
+       emailRules: [
+         email => !!email || 'required',
+         v => /.+@.+\..+/.test(v) || 'Invalid',
+       ],
+       city: [
+         'Chennai',
+         'Mumbai',
+         'Bangalore',
+         'Hyderabad',
+         'Delhi',
+         'Nagpur',
+       ],
+       checkbox: false,
+       radioGroup: 1,
+       phone:'',
+       phoneRules:[
+        phone=>!!phone || 'required',
+        v=>v.length>8 || 'Invalid number',
+      ],
+      hobbies: [],
+     }),     
+      methods: {
+        validate () {
+          this.$refs.form.validate()
+          const valid = {
+            Name : this.name,
+            Gender :this.gender,
+            Hobbies:this.hobbies,
+            City: this.city,
+            Email : this.email,
+            Phone: this.phone,
+          }
+          console.log(JSON.stringify(valid))
+        }
+     },
+   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+
+
